ce3c1a06af3d9339ea9ef6188bb4d47127a81763
Athos Martinez <athosandrade04@gmail.com>
2023-02-14 23:34:05 -0300

    .

--- CursoUdemy/COLECOES/Lista.cs
+++ CursoUdemy/COLECOES/Lista.cs
@@ -12,23 +12,34 @@ namespace CursoUdemy.COLECOES
         public string Nome;
         public double Preco;
 
-    
-
-    public Product(string nome, double preco)
-    {
-        Nome = nome;
-        Preco = preco;
+        public Product(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
     }
 
-}
-
-internal class Lista
+    class Lista
     {
-
-
         public static void Executar()
         {
+            var livro = new Product("Nome do livro", 49.9);
+            var carrinho = new List<Product>();
+            carrinho.Add(livro);
+            var combo = new List<Product>()
+            {
+                new Product("Camisa 1", 30.0),
+                new Product("Camisa 2", 50.0),
+                new Product("Camisa 3",70.0)
+            };
+            carrinho.AddRange(combo);
+            Console.WriteLine(carrinho.Count);
 
+            foreach (var item in carrinho)
+            {
+                Console.Write(carrinho.IndexOf(item));
+                Console.WriteLine($" {item.Nome} {item.Preco}");
+            }
         }
     }
 }
