666945a93a0cf28f8366bc5a26a7613356d7adb0
Athos Martinez <athosandrade04@gmail.com>
2023-02-18 02:47:09 -0300

    .

---
+++ CursoUdemy/COLECOES/Dicionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoUdemy.COLECOES
+{
+    internal class Dicionario
+    {
+        public static void Executar()
+        {
+            var filmes = new Dictionary<int, string>();
+            filmes.Add(2000, "Gladiador");
+            filmes.Add(2020, "Louco de Banana");
+            filmes.Add(2010, "Filme bacana");
+            filmes.Add(2014, "Filme das divas");
+
+            if (filmes.ContainsKey(2020))
+            {
+                Console.WriteLine("2020: " + filmes[2020]);
+                Console.WriteLine("2020: " + filmes.GetValueOrDefault(2020));
+
+            }
+
+            Console.WriteLine(filmes.ContainsValue("Filme bacana"));
+
+            Console.WriteLine($"REMOVEU {filmes.Remove(2000)}");
+
+            filmes.TryGetValue(2020, out string filme2020);
+            Console.WriteLine($"Filme: {filme2020}");
+
+            foreach (var chaves in filmes.Keys)
+            {
+                Console.WriteLine(chaves);
+            }
+
+            foreach (var valor in filmes.Values)
+            {
+                Console.WriteLine(valor);
+            }
+            foreach (KeyValuePair<int, string> filme in filmes)
+            {
+                Console.WriteLine($"{filme.Value} é de {filme.Key}");
+            }
+
+            foreach (var filme in filmes)
+            {
+                Console.WriteLine($"{filme.Value} é de {filme.Key}");
+            }
+        }
+    }
+}
---
+++ CursoUdemy/COLECOES/Pilha.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+using System.Text;
+
+
+namespace CursoUdemy.COLECOES
+{
+    internal class Pilha
+    {
+        public static void Executar()
+        {
+            var pilha = new Stack();
+            pilha.Push(3);
+            pilha.Push("a");
+            pilha.Push(true);
+            pilha.Push(5.55);
+
+            Console.WriteLine(pilha.Count);
+            foreach (var itens in pilha)
+            {
+                Console.Write($"{itens} ");
+            }
+
+            Console.WriteLine($"\nPop: {pilha.Pop()}");
+
+
+            foreach (var itens in pilha)
+            {
+                Console.Write($"{itens} ");
+            }
+
+            Console.WriteLine($"\nPeek: {pilha.Peek()}");
+            Console.WriteLine(pilha.Count);
+            foreach (var itens in pilha)
+            {
+                Console.Write($"{itens} ");
+            }
+        }
+    }
+}
