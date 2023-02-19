484e3454cc0523841ca13f5ea4e57d618d53ccc3
Athos Martinez <athosandrade04@gmail.com>
2023-02-19 17:41:36 -0300

    .

--- CursoUdemy/OO/Herança.cs
+++ CursoUdemy/OO/Herança.cs
@@ -6,11 +6,86 @@ using System.Threading.Tasks;
 
 namespace CursoUdemy.OO
 {
+    public class Carro
+    {
+        protected readonly int VelocidadeMaxima;
+        int VelocidadeAtual;
+
+        public Carro(int velocidadeMaxima)
+        {
+            VelocidadeMaxima = velocidadeMaxima;
+        }
+
+
+
+        protected int AlterarVelocidade(int delta)
+        {
+            int novaVelocidade = VelocidadeAtual + delta;
+
+            if (novaVelocidade < 0)
+            {
+                VelocidadeAtual = 0;
+
+            }
+            else if (novaVelocidade > VelocidadeMaxima)
+            {
+                VelocidadeAtual = VelocidadeMaxima;
+            }
+            else
+            {
+                VelocidadeAtual = novaVelocidade;
+            }
+            return VelocidadeAtual;
+
+
+        }
+
+        public int Acelelar()
+        {
+            return AlterarVelocidade(5);
+
+        }
+        public int Freiar()
+        {
+            return AlterarVelocidade(-5);
+        }
+    }
+
+    public class Uno : Carro
+    {
+        public Uno() : base(200)
+        {
+
+        }
+    }
+
+    public class Ferrari : Carro
+    {
+        public Ferrari() : base(370)
+        {
+        }
+    }
+
+
     internal class Herança
     {
         public static void Executar()
         {
+            Console.WriteLine("Uno...");
+            Uno carro1 = new Uno();
+            Console.WriteLine(carro1.Acelelar());
+            Console.WriteLine(carro1.Acelelar());
+            Console.WriteLine(carro1.Freiar());
+            Console.WriteLine(carro1.Freiar());
+            Console.WriteLine(carro1.Acelelar());
 
+            Console.WriteLine("Ferrari...");
+            Ferrari carro2 = new Ferrari();
+            Console.WriteLine(carro2.Acelelar());
+            Console.WriteLine(carro2.Acelelar());
+            Console.WriteLine(carro2.Freiar());
+            Console.WriteLine(carro2.Freiar());
+            Console.WriteLine(carro2.Acelelar());
         }
     }
 }
