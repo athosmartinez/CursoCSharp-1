cff14a4536bd7916bcb8213e35153c28fce1a488
athosmartinez <athosandrade04@gmail.com>
2023-02-26 18:04:11 -0300

    .

--- CursoUdemy/API/PrimeiroArquivo.cs
+++ CursoUdemy/API/PrimeiroArquivo.cs
@@ -7,11 +7,34 @@ using System.IO;
 
 namespace CursoUdemy.API
 {
+
+    public static class ExtensaoString
+    {
+
+    }
     internal class PrimeiroArquivo
     {
         public static void Executar()
         {
-            
+
+            var path = @"C:\Users\athos\primeiro_arquivo.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("Esse é");
+                    sw.WriteLine("o nosso");
+                    sw.WriteLine("primeiro");
+                    sw.WriteLine("arquivo!");
+                }
+            }
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("");
+                sw.WriteLine("pode adicioanr");
+                sw.WriteLine("mais");
+                sw.WriteLine("texto!");
+            }
         }
     }
 }
